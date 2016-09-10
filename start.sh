ca96f4acbbafd9fd71165f9fd148eef4ade444e6
Chris Fregly <chris@fregly.com>
2016-09-09 18:16:11 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-export KUBERNETES_HOME=/root/kubernetes/
-export PATH=$KUBERNETES_HOME/platforms/linux/amd64:$PATH
-
 kubectl config set-credentials demo --username=$KUBERNETES_USERNAME --password=$KUBERNETES_PASSWORD
 kubectl config set-cluster demo --insecure-skip-tls-verify=true --server=https://$KUBERNETES_SERVER
 kubectl config set-context demo --user=demo --namespace=default --cluster=demo
