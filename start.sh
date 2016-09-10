4834bb51af5bc500cb8302cdf252893821b77688
Chris Fregly <chris@fregly.com>
2016-09-09 17:15:55 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export PATH=$KUBERNETES_HOME/platforms/linux/amd64:$PATH
+
+kubectl config set-credentials demo --username=$KUBERNETES_USERNAME --password=$KUBERNETES_PASSWORD
+kubectl config set-cluster demo --insecure-skip-tls-verify=true --server=https://$KUBERNETES_SERVER
+kubectl config set-context demo --user=demo --namespace=default --cluster=demo
+kubectl config use-context demo
+kubectl get nodes
